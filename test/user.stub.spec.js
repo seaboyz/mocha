a0e441ba8d06a26e6b2d61aed360728abd1b07e6
seaboyz <seaboyz@hotmail.com>
2023-06-04 14:49:17 -0500

    stub async return promise

--- test/user.stub.spec.js
+++ test/user.stub.spec.js
@@ -1,6 +1,7 @@
 const { user } = require('../src/User.js');
 const { expect } = require('chai');
 const sinon = require('sinon');
+const { default: axios } = require('axios');
 
 describe('sync code', () => {
   it('verify the greeting message method', () => {
@@ -12,5 +13,36 @@ describe('sync code', () => {
     const greetStub = sinon.stub(user, 'greet');
     greetStub.withArgs('World').returns('Hello World');
     expect(user.greetMessage('World')).to.be.equal('Hello World');
+  });
+});
+
+describe('async code', () => {
+  it('check the user data', async () => {
+    const result = await axios.get('https://reqres.in/api/users/2');
+    const data = result.data.data;
+    expect(data.id).to.be.equal(2);
+    expect(data.email).to.be.equal('janet.weaver@reqres.in');
+    expect(data.first_name).to.be.equal('Janet');
+  });
+});
+
+describe.only('get UserDataById - stub async method', () => {
+  let stub;
+  beforeEach(() => {
+    stub = sinon.stub(user, 'getUserDataById');
+  });
+  it('stub getUserUserDataById', async () => {
+    const output = {
+      id: 2,
+      email: 'test@test.com',
+      first_name: 'john',
+    };
+
+    stub.withArgs(2).returns(Promise.resolve(output));
+
+    const data = await user.getUserDataById(2);
+    expect(data.id).to.be.equal(output.id);
+    expect(data.email).to.be.equal(output.email);
+    expect(data.first_name).to.be.equal(output.first_name);
   });
 });
