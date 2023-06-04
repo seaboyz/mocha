a38906af8aa6f9be7862cb00a75954910db2a165
seaboyz <seaboyz@hotmail.com>
2023-06-04 10:40:18 -0500

    before beforeEach after afterEach

--- test/app.spec.js
+++ test/app.spec.js
@@ -2,6 +2,18 @@ const { expect } = require('chai');
 const { add, sub } = require('../src/app.js');
 
 describe('suite 1', () => {
+  afterEach(() => {
+    console.log('after each');
+  });
+  beforeEach(() => {
+    console.log('before each');
+  });
+  after(() => {
+    console.log('after');
+  });
+  before(() => {
+    console.log('before');
+  });
   it('add(2,3) should return 5', () => {
     expect(add(2, 3)).to.equal(5);
   });
