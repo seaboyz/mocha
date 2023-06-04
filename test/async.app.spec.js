77d87456102fe4807b5d2ff7d315105f70513ef3
seaboyz <seaboyz@hotmail.com>
2023-06-04 10:59:48 -0500

    test async using done function

--- test/async.app.spec.js
+++ test/async.app.spec.js
@@ -9,4 +9,14 @@ describe('async test suite', () => {
         expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in')
       );
   });
+
+  it('done based way', done => {
+    axios
+      .get('https://reqres.in/api/users/2')
+      .then(res => {
+        expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+        done();
+      })
+      .catch(console.log);
+  });
 });
